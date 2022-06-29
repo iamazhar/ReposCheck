f5e30580f2c0d39f69d7c5c37271c5b7ec5d5c32
Azhar <azharcodes@gmail.com>
2022-06-29 13:30:16 +0530

    :sparkles: adds mvp

--- ReposCheck/ContentView.swift
+++ ReposCheck/ContentView.swift
@@ -6,16 +6,66 @@
 //
 
 import SwiftUI
+import ColorizeSwift
+
+final class GitStatusHelper {
+  public static func runGitStatusScript() -> String {
+    let pipe = Pipe()
+    
+    let file = pipe.fileHandleForReading
+    
+    let process = Process()      
+    process.launchPath = "/bin/sh"
+    
+    process.currentDirectoryPath = "~/Documents/GitHub/" 
+    
+    process.arguments = [ "-c", "~/Documents/GitHub/repos-status.sh"]
+    
+    process.standardOutput = pipe
+    process.launch()
+    
+    let data: Data = file.readDataToEndOfFile()
+    
+    let gitStatusOutput = String(data: data, encoding: .utf8)
+    
+    print("Git status: \n\n", gitStatusOutput ?? "")
+    
+    let firstPass = gitStatusOutput?.replacingOccurrences(of: "[0;35m", with: "")
+    let secondPass = firstPass?.replacingOccurrences(of: "[0;31m", with: "")
+    let thirdPass = secondPass?.replacingOccurrences(of: "[0m", with: "")
+    
+    return thirdPass ?? ""
+  }
+}
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var gitCheckOutput: String = ""
+  var body: some View {
+    VStack(alignment: .leading, spacing: 24.0) {
+      
+      Text(gitCheckOutput)
+        .font(
+          Font
+            .system(size: 14)
+            .monospaced()
+        )
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      
+      Button("Check again") {
+        gitCheckOutput = GitStatusHelper.runGitStatusScript()
+      }
+      .padding()
+      
     }
+    .padding()
+    .onAppear { 
+      gitCheckOutput = GitStatusHelper.runGitStatusScript()
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
