bde59a964043b67dd289b54e43138a1d32afa962
Azhar <azharcodes@gmail.com>
2022-06-29 12:14:20 +0530

    Initial Commit

---
+++ ReposCheck/ReposCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReposCheckApp.swift
+//  ReposCheck
+//
+//  Created by Azhar Anwar on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ReposCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
