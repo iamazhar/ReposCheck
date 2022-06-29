a5844b321bd13c76cbdb27c2b99bf77d9423cc96
Azhar <azharcodes@gmail.com>
2022-06-29 14:34:06 +0530

    adds support for directory and script path input

--- ReposCheck/ContentView.swift
+++ ReposCheck/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 final class GitStatusHelper {
-  public static func runGitStatusScript() -> String {
+  public static func runGitStatusScript(
+    withDirectoryPath directoryPath: String = "~/Documents/GitHub/",
+    withScriptPath scriptPath: String = "~/Documents/GitHub/repos-status.sh"
+  ) -> String {
     let pipe = Pipe()
     
     let file = pipe.fileHandleForReading
@@ -16,9 +19,9 @@ final class GitStatusHelper {
     let process = Process()      
     process.launchPath = "/bin/sh"
     
-    process.currentDirectoryPath = "~/Documents/GitHub/" 
+    process.currentDirectoryPath = directoryPath
     
-    process.arguments = [ "-c", "~/Documents/GitHub/repos-status.sh"]
+    process.arguments = [ "-c", scriptPath]
     
     process.standardOutput = pipe
     process.launch()
@@ -31,34 +34,76 @@ final class GitStatusHelper {
     
     let firstPass = gitStatusOutput?.replacingOccurrences(of: "[0;35m", with: "")
     let secondPass = firstPass?.replacingOccurrences(of: "[0;31m", with: "")
-    let thirdPass = secondPass?.replacingOccurrences(of: "[0m", with: "")
     
-    return thirdPass ?? ""
+    guard let thirdPass = secondPass?.replacingOccurrences(of: "[0m", with: "") else { return "" }
+    
+    return thirdPass.isEmpty ? "Check if script path is correct" : thirdPass
   }
 }
 
 struct ContentView: View {
   @State var gitCheckOutput: String = ""
+  @State private var directoryPath: String = "~/Documents/GitHub/"
+  @State private var scriptPath: String = "~/Documents/GitHub/repos-status.sh"
+  
   var body: some View {
     VStack(alignment: .leading, spacing: 24.0) {
       
-      Text(gitCheckOutput)
-        .font(
-          Font
-            .system(size: 14)
-            .monospaced()
-        )
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      VStack(alignment: .leading) {
+        
+        VStack(alignment: .leading, spacing: 8.0) { 
+          Text("Directory path")
+            .font(.title2)
+          
+          TextField("Enter directory path", text: $directoryPath)
+            .padding()
+            .onSubmit {
+              checkGitRepos()
+            }
+        }
+        
+        VStack(alignment: .leading, spacing: 8.0) { 
+          Text("Script path")
+            .font(.title2)
+          
+          TextField("Enter script path", text: $scriptPath)
+            .padding()
+            .onSubmit {
+              checkGitRepos()
+            }
+        }
+      }
+      .padding()
+      
+      ScrollView(.vertical, showsIndicators: true) { 
+        Text(gitCheckOutput)
+          .font(
+            Font
+              .system(size: 14)
+              .monospaced()
+          )
+          .transition(.slide)
+          .id("gitCheckOutput" + gitCheckOutput)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+      }
       
       Button("Check again") {
-        gitCheckOutput = GitStatusHelper.runGitStatusScript()
+        checkGitRepos()
       }
       .padding()
       
     }
     .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
     .onAppear { 
-      gitCheckOutput = GitStatusHelper.runGitStatusScript()
+      checkGitRepos()
+    }
+  }
+  
+  private func checkGitRepos() {
+    gitCheckOutput = "Checking..."
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { 
+      self.gitCheckOutput = GitStatusHelper.runGitStatusScript(withDirectoryPath: directoryPath, withScriptPath: scriptPath)
     }
   }
 }
